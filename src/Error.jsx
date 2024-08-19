ecc32335d759e3fa6e93c36f02582a952da63933
Shaaz-Ahmed <shaazmukadam1616@gmail.com>
2024-08-19 23:14:49 +0530

    code of error page

--- src/Error.jsx
+++ src/Error.jsx
@@ -4,6 +4,7 @@ const Error = () =>{
     return(
         <>
             <h1> Hello this is Error Page</h1>
+            <h1> Hello this is Error Page</h1>
         </>,
         document.getElementById("root")
     );
